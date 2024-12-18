1acaac4d8b89b81bc79c24cc7bf200ca47c326db
Ajay Krishna Nalisetty <ajaykn@microsoft.com>
2024-12-18 09:17:38 +0900

    update to GA version

--- internal/services/mdp/client/client.go
+++ internal/services/mdp/client/client.go
@@ -3,7 +3,7 @@ package client
 import (
 	"fmt"
 
-	"github.com/hashicorp/go-azure-sdk/resource-manager/devopsinfrastructure/2024-04-04-preview/pools"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/devopsinfrastructure/2024-10-19/pools"
 
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
--- internal/services/mdp/mdp_pool.go
+++ internal/services/mdp/mdp_pool.go
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/identity"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
-	"github.com/hashicorp/go-azure-sdk/resource-manager/devopsinfrastructure/2024-04-04-preview/pools"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/devopsinfrastructure/2024-10-19/pools"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
